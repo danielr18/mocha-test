addadcf68895e5e333a218fa62aa5ef319070a35
you@example.com <you@example.com>
2016-01-22 09:54:24 -0430

    Initial commit

--- test/mocha.js
+++ test/mocha.js
@@ -1,8 +1,12 @@
 describe("Color Code Converter", function() {
     describe("RGB to Hex Conversion", function() {
+        it("converts the basic colors", function() {
 
+        });
     });
     describe("Hex to RGB Conversion", function() {
+        it("converts the basic colors", function() {
 
+        });
     });
 });
